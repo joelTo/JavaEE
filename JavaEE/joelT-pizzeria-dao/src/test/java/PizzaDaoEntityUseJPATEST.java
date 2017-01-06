0da28350517b338d5285894d00e21d715f5032b1
joel T <joel.torib@gmail.com>
2017-01-06 11:40:23 +0100

    Spring JPA --> creation entityManager + plateform transaction + Rajout du Persistence.xml + .... + testFindAll()

---
+++ JavaEE/joelT-pizzeria-dao/src/test/java/PizzaDaoEntityUseJPATEST.java
@@ -0,0 +1,25 @@
+import javax.inject.Inject;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import fr.dao.PizzaDaoEntityUseJPA;
+import fr.model.CategoriePizza;
+import fr.model.Pizza;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = SpringDAOTestConfig.class)
+public class PizzaDaoEntityUseJPATEST {
+	@Inject
+	private PizzaDaoEntityUseJPA pizzaDao;
+
+	@Test
+	public void test() {
+		Assert.assertEquals((pizzaDao.findAllPizza().stream().filter(x -> "PEP".equals(x.getCode())).findAny().get()),
+				new Pizza(1, "PEP", "PEPERONI", CategoriePizza.valueOf("VIANDE"), 11.2));
+	}
+
+}
