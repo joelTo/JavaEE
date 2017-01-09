48a3717e103184c945963963a5a431c1fe3ba8a5
joel T <joel.torib@gmail.com>
2017-01-09 18:28:21 +0100

    MVC -- ajout

--- JavaEE/joelT-pizzeria-stock-app/src/main/java/fr/pizzeria/spring/mvc/controller/TestController.java
+++ JavaEE/joelT-pizzeria-stock-app/src/main/java/fr/pizzeria/spring/mvc/controller/TestController.java
@@ -1,15 +1,24 @@
 package fr.pizzeria.spring.mvc.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import fr.model.Ingredient;
 
 @Controller
 public class TestController {
+	@Autowired
+	IngredientDaoDatabase ingredientDao;
 
 	@RequestMapping(value = "/test", method = RequestMethod.GET)
-	public String printWelcome() {
-		return "hello";
-
+	public ModelAndView printWelcome() {
+		ModelAndView mav = new ModelAndView();
+		List<Ingredient> people = ingredientDao.getPeople();
+		mav.addObject("people", people);
+		mav.setViewName("list");
+		return mav;
 	}
 }
